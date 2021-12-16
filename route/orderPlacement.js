9dbebee25be133e2ea9814f3f09b478d44c89df1
Muhammad Ismail <mi477048@gmail.com>
2021-12-16 10:34:13 +0500

    get and place order api

--- route/orderPlacement.js
+++ route/orderPlacement.js
@@ -5,7 +5,7 @@ const verify = require("../verifyToken");
 
 //post order in the DB
 
-router.post("/order", async (req, res) => {
+router.post("/newOrder", async (req, res) => {
   const newOrder = new order({
     customer_id: req.body.customer_id,
     order_type: req.body.order_type,
@@ -19,17 +19,15 @@ router.post("/order", async (req, res) => {
   }
 });
 
-//GET all order
+//GET all order about a specfic customer
 
 router.get("/find/:id", async (req, res) => {
   try {
-    const user = await User.findById(req.params.id);
-    const { password, ...info } = user._doc;
-    res.status(200).json(info);
+    const orders = await order.find({ customer_id: req.params.id });
+    res.status(200).json(orders);
   } catch (err) {
     res.status(500).json(err);
   }
 });
-
 
 module.exports = router;
