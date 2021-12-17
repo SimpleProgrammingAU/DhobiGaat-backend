68e97772c728d1e627e3755383608c25ddfd3729
Muhammad Ismail <mi477048@gmail.com>
2021-12-17 17:37:07 +0500

    admin api added

--- route/earningManipulate.js
+++ route/earningManipulate.js
@@ -2,9 +2,50 @@ const router = require("express").Router();
 const Earning = require("../models/earning");
 const jwt = require("jsonwebtoken");
 
-//post admin earning
+//post admin earning for the first time
 
 router.post("/earning", async (req, res) => {
+  const newEarning = new Earning({
+    admin_id: req.body.admin_id,
+    earning: req.body.earning,
+  });
+  try {
+    const earning = await newEarning.save();
+    res.status(201).json(earning);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// get earning of admin whthere he has earning or not
+
+router.get("/earning/:id", async (req, res) => {
+  try {
+    const oldEarning = await Earning.find({ admin_id: req.params.id });
+    res.status(201).json(oldEarning);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// upadte earning of admin
+
+router.put("/:id", async (req, res) => {
+  const oldEarning = await Earning.findById(req.params.id);
+  if (oldEarning) {
+    req.body.earning += oldEarning.earning;
+    const newEarning = new Earning({
+      admin_id: req.body.admin_id,
+      earning: req.body.earning,
+    });
+    try {
+      const earning = await newEarning.findByIdAndUpdate(req.params.id);
+      res.status(201).json(earning);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  }
+
   const newEarning = new Earning({
     admin_id: req.body.admin_id,
     earning: req.body.earning,
