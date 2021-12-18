dc25bcf8c43c0f131d878a372a73353edd5562d1
Muhammad Ismail <mi477048@gmail.com>
2021-12-18 13:56:33 +0500

    service and price of the service api added

--- models/services.js
+++ models/services.js
@@ -3,11 +3,78 @@ const mongoose = require("mongoose");
 const servicesSchema = new mongoose.Schema(
   {
     admin_id: { type: String, required: true, unique: true },
-    service1: { type: String, defaut: "" },
-    service2: { type: String, defaut: "" },
-    service3: { type: String, defaut: "" },
-    service4: { type: String, defaut: "" },
-    service5: { type: String, defaut: "" },
+
+    service1: [
+      {
+        service: {
+          type: String,
+          default: "",
+        },
+        price: {
+          type: Number,
+          default: "Number",
+        },
+      },
+    ],
+    service2: [
+      {
+        service: {
+          type: String,
+          default: "",
+        },
+        price: {
+          type: Number,
+          default: "Number",
+        },
+      },
+    ],
+    service3: [
+      {
+        service: {
+          type: String,
+          default: "",
+        },
+        price: {
+          type: Number,
+          default: "Number",
+        },
+      },
+    ],
+
+    service4: [
+      {
+        service: {
+          type: String,
+          default: "",
+        },
+        price: {
+          type: Number,
+          default: "Number",
+        },
+      },
+    ],
+    service5: [
+      {
+        service: {
+          type: String,
+          default: "",
+        },
+        price: {
+          type: Number,
+          default: "Number",
+        },
+      },
+    ],
+
+    // service2: { type: String, defaut: "" },
+    // service3: { type: String, defaut: "" },
+    // service4: { type: String, defaut: "" },
+    // service5: { type: String, defaut: "" },
+
+    // service2: { type: Array, defaut: [] },
+    // service3: { type: Array, defaut: [] },
+    // service4: { type: Array, defaut: [] },
+    // service5: { type: Array, defaut: [] },
   },
   { timestamps: true }
 );
