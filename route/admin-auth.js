1eaade3913d431f8cef28520bb61a1a5acdcd49a
Muhammad Ismail <mi477048@gmail.com>
2021-12-21 23:36:38 +0500

    user and dhobie-admin crud api added

--- route/admin-auth.js
+++ route/admin-auth.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const adminUser = require("../models/Admin");
 const CryptoJS = require("crypto-js");
 const jwt = require("jsonwebtoken");
+const verify = require("../verifyToken");
 
 //REGISTER
 
@@ -44,6 +45,53 @@ router.post("/login", async (req, res) => {
     const { password, ...info } = user._doc;
 
     res.status(200).json({ ...info, accessToken });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// DELETE admin account
+
+router.delete("/delete/:id", async (req, res) => {
+  try {
+    await adminUser.findByIdAndDelete(req.params.id);
+    res.status(200).json("Admin account has been deleted...");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Update admin account
+
+router.put("/update/:id", async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.SECRET_KEY
+    ).toString();
+  }
+
+  try {
+    const updatedUser = await adminUser.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//GET customer or admin-dhobie information
+
+router.get("/find/:id", async (req, res) => {
+  try {
+    const user = await adminUser.findById(req.params.id);
+    const { password, ...info } = user._doc;
+    res.status(200).json(info);
   } catch (err) {
     res.status(500).json(err);
   }
--- route/user-auth.js
+++ route/user-auth.js
@@ -24,6 +24,7 @@ router.post("/register", async (req, res) => {
 });
 
 //LOGIN
+
 router.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({ email: req.body.email });
@@ -42,6 +43,53 @@ router.post("/login", async (req, res) => {
     const { password, ...info } = user._doc;
 
     res.status(200).json({ ...info, accessToken });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// DELETE user account
+
+router.delete("/delete/:id", async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("User account has been deleted...");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Update admin account
+
+router.put("/update/:id", async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.SECRET_KEY
+    ).toString();
+  }
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//GET customer or admin-dhobie information
+
+router.get("/find/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...info } = user._doc;
+    res.status(200).json(info);
   } catch (err) {
     res.status(500).json(err);
   }
